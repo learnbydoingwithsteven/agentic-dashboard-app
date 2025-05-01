9cbb9a2f8d7c7464cbc790c8f0bb35f37a331cb8
learnbydoingwithsteven <168478790+learnbydoingwithsteven@users.noreply.github.com>
2025-05-01 20:08:15 +0200

    Update set_env.ps1

--- backend/set_env.ps1
+++ backend/set_env.ps1
@@ -1,5 +1,5 @@
 # Set the GROQ_API_KEY environment variable
-$env:GROQ_API_KEY = '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'
+$env:GROQ_API_KEY = ''
 
 # Verify the environment variable is set
 Write-Host "GROQ_API_KEY set to: $env:GROQ_API_KEY"
